56a9fdb866b0bd588c065a76488cd2c58cbaa6ce
Daniel Angulo <dangulo4@gmail.com>
2020-04-05 01:48:59 -0700

    added page model and finalized user model with seeded data for testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,19 @@
-
-
-INSERT INTO users
-    (name)
-VALUES('Alexia Chalita');
+USE minutepage;
 
 INSERT INTO users
-    (name)
-VALUES('Matthew Bell');
+    (username, firstname, lastname, email, password, createdAt, updatedAt)
+VALUES
+    ('user1', 'Alexia', 'Chalita', 'password', 'user1@gmail.com', curdate(), curdate()),
+    ('user2', 'Matthew', 'Bell', 'password', 'user2@gmail.com', curdate(), curdate()),
+    ('user3', 'Daniel', 'Angulo', 'password', 'user3@gmail.com', curdate(), curdate())
+;
 
-INSERT INTO users
-    (name)
-VALUES('Daniel Angulo');
+INSERT INTO pages
+    (name, header, description, title, quote, pointOne, pointTwo, pointThree, pointFour, pointFive, createdAt, updatedAt, UserId)
+VALUES
+    ('MyLandingPage', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, autem?', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta',
+        'My Title', 'Lorem, ipsum dolor sit amet consectetur adipisicing', 'Lorem, ipsum dolor sit amet', curdate(), curdate(), 1),
+    ('MyLandingPage', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, autem?', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta',
+        'My Title', 'Lorem, ipsum dolor sit amet consectetur adipisicing', 'Lorem, ipsum dolor sit amet', 'Lorem ipsum dolor sit.', 'Lorem ipsum dolor sit.',
+        'Lorem ipsum dolor sit.', 'Lorem ipsum dolor sit.', curdate(), curdate(), 2)
+;
