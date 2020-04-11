822380c76e833dfaf987c408f0a6798e8c515bea
Daniel Angulo <dangulo4@gmail.com>
2020-04-11 12:45:27 -0700

    document ready for sign on

--- public/js/script.js
+++ public/js/script.js
@@ -1,51 +1,54 @@
-const close = document.getElementById('close');
-const open = document.getElementById('signUp');
-const modal = document.getElementById('modal');
+$(document).ready(function () {
+  const close = document.getElementById('close');
+  const open = document.getElementById('signUp');
+  const modal = document.getElementById('modal');
 
-// Show modal
-open.addEventListener('click', () => modal.classList.add('show-modal'));
+  // Show modal
+  open.addEventListener('click', () => modal.classList.add('show-modal'));
 
-// Hide modal
-close.addEventListener('click', () => modal.classList.remove('show-modal'));
+  // Hide modal
+  close.addEventListener('click', () => modal.classList.remove('show-modal'));
 
-// Hide modal on outside click
-window.addEventListener('click', (e) =>
-  e.target == modal ? modal.classList.remove('show-modal') : false
-);
+  // Hide modal on outside click
+  window.addEventListener('click', (e) =>
+    e.target == modal ? modal.classList.remove('show-modal') : false
+  );
 
-// When user clicks add-btn
-$('.submit-btn').on('click', function (event) {
-  // event.preventDefault();
+  // When user clicks add-btn
+  $('.submit-btn').on('click', function (event) {
+    event.preventDefault();
 
-  // Make a newUser object
-  var newUser = {
-    username: $('#user-name').val().trim(),
-    firstname: $('#first-name').val().trim(),
-    lastname: $('#last-name').val().trim(),
-    email: $('#email').val().trim(),
-    password: $('#password').val().trim(),
-  };
+    // Make a newUser object
+    var newUser = {
+      username: $('#user-name').val().trim(),
+      firstname: $('#first-name').val().trim(),
+      lastname: $('#last-name').val().trim(),
+      email: $('#email').val().trim(),
+      password: $('#password').val().trim(),
+    };
 
-  console.log(newUser);
+    console.log(newUser);
 
-  // Send an AJAX POST-request with jQuery
-  $.post('/api/users', newUser)
-    // On success, run the following code
-    .then(function () {
-      var row = $('<div>');
-      row.addClass('user');
+    // Send an AJAX POST-request with jQuery
+    $.post('/api/users', newUser)
+      // On success, run the following code
+      .then(function () {
+        var row = $('<div>');
+        row.addClass('user');
 
-      row.append('<p>' + newUser.username + ' users: </p>');
-      row.append('<p>' + newUser.body + '</p>');
+        row.append('<p>' + newUser.username + ' users: </p>');
+        row.append('<p>' + newUser.body + '</p>');
 
-      $('#user-area').prepend(row);
-    });
+        $('#user-area').prepend(row);
+      });
 
-  // Empty each input box by replacing the value with an empty string
-  $('#user-name').val('');
-  $('#first-name').val('');
-  $('#last-name').val('');
-  $('#email').val('');
-  $('#password').val('');
-  $('#password2').val('');
+    // Empty each input box by replacing the value with an empty string
+    $('#user-name').val('');
+    $('#first-name').val('');
+    $('#last-name').val('');
+    $('#email').val('');
+    $('#password').val('');
+    $('#password2').val('');
+    window.location.href = 'mainscreen.html';
+  });
 });
