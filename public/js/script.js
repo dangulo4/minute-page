4abf688e12f470f549bdfcee27b5632dcb17da26
Daniel Angulo <dangulo4@gmail.com>
2020-04-10 21:10:17 -0700

    added functionality to capture user data entered in sign on modal

--- public/js/script.js
+++ public/js/script.js
@@ -12,3 +12,40 @@ close.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) =>
   e.target == modal ? modal.classList.remove('show-modal') : false
 );
+
+// When user clicks add-btn
+$('.submit-btn').on('click', function (event) {
+  // event.preventDefault();
+
+  // Make a newUser object
+  var newUser = {
+    username: $('#user-name').val().trim(),
+    firstname: $('#first-name').val().trim(),
+    lastname: $('#last-name').val().trim(),
+    email: $('#email').val().trim(),
+    password: $('#password').val().trim(),
+  };
+
+  console.log(newUser);
+
+  // Send an AJAX POST-request with jQuery
+  $.post('/api/users', newUser)
+    // On success, run the following code
+    .then(function () {
+      var row = $('<div>');
+      row.addClass('user');
+
+      row.append('<p>' + newUser.username + ' users: </p>');
+      row.append('<p>' + newUser.body + '</p>');
+
+      $('#user-area').prepend(row);
+    });
+
+  // Empty each input box by replacing the value with an empty string
+  $('#user-name').val('');
+  $('#first-name').val('');
+  $('#last-name').val('');
+  $('#email').val('');
+  $('#password').val('');
+  $('#password2').val('');
+});
