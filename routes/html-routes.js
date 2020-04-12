7c6a17d502d9940608b186195364b17e67a526e6
Daniel Angulo <dangulo4@gmail.com>
2020-04-11 23:52:00 -0700

    pushing to share with team

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,32 +4,31 @@
 
 // Dependencies
 // =============================================================
-var path = require("path");
+var path = require('path');
 
 // Routes
 // =============================================================
-  // Each of the routes determines which HTML page the user is sent to.
-  module.exports = function(app) {
-
+// Each of the routes determines which HTML page the user is sent to.
+module.exports = function (app) {
   // login route loads index.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  // user route loads mainscreen.html  
+  // user route loads mainscreen.html
   //==This page is where users can see their landing pages, and there is a button to start making a new one==
-  app.get("/user", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/mainscreen.html"));
+  app.get('/user', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/mainscreen.html'));
   });
 
   // questions route loads questions.html
-  app.get("/questions", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/questions.html"));
+  app.get('/questions', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/questions.html'));
   });
 
   // processing route loads existingpages.html
-  app.get("/gallery", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/existingpages.html"));
+  app.get('/gallery', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/existingpages.html'));
   });
 
   // // output route loads output.html
@@ -38,8 +37,7 @@ var path = require("path");
   // });
 
   // logout route loads logout.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/logout.html"));
- });
-
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/logout.html'));
+  });
 };
